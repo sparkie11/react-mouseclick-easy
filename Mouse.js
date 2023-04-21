b01934f2b74081dcefe0c521c9b7ff4547800313
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-04-21 11:38:54 +0530

    Add files via upload
    just create a new css file..

---
+++ Mouse.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./head.css";
+
+const Mouse = () => {
+  const [open, Setopen] = useState(false);
+  const menus = ["whitebeard", "Blackbeard", "kaido", "lily", "Redhat"];
+  return (
+    <div className="luffy">
+      <h1>yo! click this ----</h1>
+      <button onClick={() => Setopen(!open)}> GOMU GOMU NOMI</button>
+      {open && (
+        <div className="roronoa">
+          <ul>
+            {menus.map((men) => (
+              <li key={men}>{men}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Mouse;
